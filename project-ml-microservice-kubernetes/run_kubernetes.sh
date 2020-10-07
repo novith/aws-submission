ec23ce8faaa3363aedf2e117e611c7a4177595fe
Mainampati Reddy <novith@gmail.com>
2020-10-06 23:15:26 -0500

    Project 4 Submission
    ML APP

---
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+
+dockerpath="mreddy7/mlapiproject"
+
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run productionmlapi --image=mreddy7/mlapiproject --port=80
+
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+
+kubectl port-forward productionmlapi 8000:80 
